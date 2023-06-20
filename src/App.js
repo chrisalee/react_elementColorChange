80269dc08010317cdda84b701ee8db3e2da84b16
chris <chrisalee222@gmail.com>
2023-06-20 12:29:14 -0500

    element color changer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
 
-function App() {
+const App = () => {
+  const [listColor, setListColor] = useState([1, 2, 3, 4, 5]);
+  const ref = useRef(null);
+
+  const handleToggleClasslistRef = (ref) => {
+    if (!ref.current) {
+      return;
+    }
+    if (!ref.current.classList.contains("big-border")) {
+      ref.current.classList.add("big-border");
+    } else {
+      ref.current.classList.remove("big-border");
+      ref.current = null;
+    }
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      handleToggleClasslistRef(ref);
+    };
+    const element = document.getElementById("wrapper");
+    element.addEventListener("click", handleOutsideClick);
+    return () => {
+      element.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="wrapper">
+        {listColor.map((item) => {
+          return (
+            <div className="item-color-wrapper">
+              <div
+                className="item-color"
+                onClick={(e) => {
+                  handleToggleClasslistRef(ref);
+                  e.stopPropagation();
+                  ref.current = e.target;
+                  handleToggleClasslistRef(ref);
+                }}
+              ></div>
+            </div>
+          );
+        })}
+        <div className="item-color-wrapper">
+          <div
+            className="item-color"
+            onClick={() => {
+              setListColor((oldValue) => [...oldValue, oldValue.length]);
+            }}
+          >
+            +
+          </div>
+        </div>
+      </div>
+      <div className="color-picker-wrapper">
+        <input
+          type="color"
+          id="head"
+          name="head"
+          onChange={(e) => {
+            e.stopPropagation();
+            if (ref.current) {
+              ref.current.style.backgroundColor = e.target.value;
+            }
+          }}
+        />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
